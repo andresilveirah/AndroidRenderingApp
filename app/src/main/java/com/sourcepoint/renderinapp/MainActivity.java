1b849121de3033d63c2e6f365531e2aa69da4fa8
André Herculano <andresilveirah@gmail.com>
2022-03-31 18:17:38 +0200

    add local pm url

--- app/src/main/java/com/sourcepoint/renderinapp/MainActivity.java
+++ app/src/main/java/com/sourcepoint/renderinapp/MainActivity.java
@@ -6,6 +6,10 @@ import android.webkit.WebView;
 
 import com.sourcepoint.renderinapp.databinding.ActivityMainBinding;
 
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     WebView webview;
@@ -16,8 +20,20 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         webview = findViewById(R.id.webview);
+        
         webview.getSettings().setLoadsImagesAutomatically(true);
         webview.getSettings().setJavaScriptEnabled(true);
-        webview.loadUrl("https://preprod-cdn.privacy-mgmt.com/privacy-manager-ott/index.html?message_id=623981");
+
+        String preProdRenderingAppHost = "https://preprod-cdn.privacy-mgmt.com";
+        String localRenderingAppHost = "http://192.168.0.10:8080";
+        String ottPMPath = "/privacy-manager-ott/index.html";
+        String messageId = "623981";
+
+        webview.loadUrl(
+                localRenderingAppHost       +
+                ottPMPath                   +
+                "?message_id=" + messageId  +
+                "&mms_origin=https://cdn.privacy-mgmt.com/mms/v2&cmpv2_origin=https://cdn.privacy-mgmt.com/consent/tcfv2"
+        );
     }
 }
