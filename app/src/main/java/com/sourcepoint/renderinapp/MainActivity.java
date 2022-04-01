6e6d0fc79622272b40e544b6e15dba6ab677328f
Sourcepoint <carmelo@sourcepoint.com>
2022-04-01 12:12:17 +0200

    compiling

--- app/src/main/java/com/sourcepoint/renderinapp/MainActivity.java
+++ app/src/main/java/com/sourcepoint/renderinapp/MainActivity.java
@@ -4,24 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
-import com.sourcepoint.renderinapp.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
     WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
         webview = findViewById(R.id.webview);
 
         webview.getSettings().setLoadsImagesAutomatically(true);
         webview.getSettings().setJavaScriptEnabled(true);
 
         // MARK Papito, here's the magic number we were all looking for.
-        webview.setInitialScale(230);
+        webview.setInitialScale(100);
 
         String preProdRenderingAppHost = "https://preprod-cdn.privacy-mgmt.com";
         String localRenderingAppHost = "http://192.168.0.10:8080";
