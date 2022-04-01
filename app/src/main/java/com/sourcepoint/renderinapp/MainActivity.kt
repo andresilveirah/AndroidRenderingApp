836021f03709358bf7dd95568a8036539a3d477c
André Herculano <andresilveirah@gmail.com>
2022-04-01 16:51:48 +0200

    change factor method

--- app/src/main/java/com/sourcepoint/renderinapp/MainActivity.kt
+++ app/src/main/java/com/sourcepoint/renderinapp/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
+    private fun getScaleFactor() : Int{
+        return resources.displayMetrics.densityDpi / 2
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,10 +16,7 @@ class MainActivity : AppCompatActivity() {
         webview.settings.loadsImagesAutomatically = true
         webview.settings.javaScriptEnabled = true
 
-        // MARK Papito, here's the magic number we were all looking for.
-        val density = getDensity()
-        val scaleFactor = density - (density * 0.6).toInt()
-        webview.setInitialScale(scaleFactor)
+        webview.setInitialScale(getScaleFactor())
 
         val preProdRenderingAppHost = "https://preprod-cdn.privacy-mgmt.com"
         val ottPMPath = "/privacy-manager-ott/index.html"
@@ -27,13 +27,5 @@ class MainActivity : AppCompatActivity() {
                 "?message_id=" + messageId +
                 "&mms_origin=https://cdn.privacy-mgmt.com/mms/v2&cmpv2_origin=https://cdn.privacy-mgmt.com/consent/tcfv2"
         )
-    }
-
-    fun getDensity() : Int{
-        val densityInDpi = resources.displayMetrics.densityDpi
-
-        println("densityInDpi $densityInDpi")
-
-        return densityInDpi
     }
 }
