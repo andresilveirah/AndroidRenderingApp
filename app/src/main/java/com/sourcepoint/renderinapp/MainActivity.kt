0dba669b601b1707fed1b239baa7b6cdaec291d1
Sourcepoint <carmelo@sourcepoint.com>
2022-04-01 12:20:40 +0200

    kotlin

---
+++ app/src/main/java/com/sourcepoint/renderinapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.sourcepoint.renderinapp
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webview = findViewById<WebView>(R.id.webview)
+        webview.settings.loadsImagesAutomatically = true
+        webview.settings.javaScriptEnabled = true
+
+        // MARK Papito, here's the magic number we were all looking for.
+        webview.setInitialScale(100)
+
+        val preProdRenderingAppHost = "https://preprod-cdn.privacy-mgmt.com"
+        val ottPMPath = "/privacy-manager-ott/index.html"
+        val messageId = "623981"
+        webview.loadUrl(
+            preProdRenderingAppHost +
+                ottPMPath +
+                "?message_id=" + messageId +
+                "&mms_origin=https://cdn.privacy-mgmt.com/mms/v2&cmpv2_origin=https://cdn.privacy-mgmt.com/consent/tcfv2"
+        )
+    }
+}
